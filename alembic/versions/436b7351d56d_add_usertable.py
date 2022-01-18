1b8671933c6b111f489464e8e45abdaebcdac533
Anton Nikulov <anton.nikulov@gmail.com>
2022-01-18 13:29:56 +0400

    initial commit

---
+++ alembic/versions/436b7351d56d_add_usertable.py
@@ -0,0 +1,24 @@
+"""add usertable
+
+Revision ID: 436b7351d56d
+Revises: 0ce101b9e2fd
+Create Date: 2022-01-18 12:26:44.407743
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '436b7351d56d'
+down_revision = '0ce101b9e2fd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ example.py
@@ -0,0 +1,25 @@
+{
+    "Post": {
+        "id": 42,
+        "content": "More much fun",
+        "created_at": "2022-01-18T09:39:12.968399+04:00",
+        "title": "Welcome to funland one more time",
+        "published": true,
+        "owner_id": 18
+    },
+    "votes": 1
+},
+
+{
+    "title": "Welcome to funland one more time",
+    "content": "More much fun",
+    "published": true,
+    "id": 39,
+    "created_at": "2022-01-18T09:38:50.799983+04:00",
+    "owner_id": 17,
+    "owner": {
+        "id": 17,
+        "email": "aaa@gmail.com",
+        "created_at": "2022-01-18T09:35:48.747190+04:00"
+    }
+},
